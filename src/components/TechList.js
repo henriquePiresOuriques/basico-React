ddad69ce79dd6a4af585fa42563cb2b4613a6976
henriquePiresOuriques <henriquepiresouriques@gmail.com>
2020-04-09 18:53:48 -0300

    conceitos básicos

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   }; 
+
+  // Executa assim que o componente aparece em tela 
+  componentDidMount() { 
+    const techs =localStorage.getItem('techs');
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillMount() {
+
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
@@ -32,10 +51,11 @@ class TechList extends Component {
       <form onSubmit={this.handleSubmit}>
         <ul>
           {this.state.techs.map(tech => (
-            <li key={tech}>
-              {tech}
-              <button onClick={() => this.handleDelete(tech)} type="button">Remover</button>
-            </li>
+            <TechItem 
+              key={tech} 
+              tech={tech}  
+              onDelete={() => this.handleDelete(tech)} 
+            />
           ))}
         </ul>
         <input 
